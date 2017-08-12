c242bb0f5880880b9403bf7abc9b30d2c005d247
Chris Nyaga <nyagachris75@gmail.com>
2017-08-12 23:24:19 +0300

    Routes for profile, messages and username added to router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :profile, only: :index
   root 'main#index'
+  resources :messages, only: :create
+  get '/:username', to: 'users#show'
+  # resources :users, only: :show
 end
