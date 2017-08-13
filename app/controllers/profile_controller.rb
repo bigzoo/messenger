ae3e87055e9936f4d314c58ce9b239e963585257
Chris Nyaga <nyagachris75@gmail.com>
2017-08-13 17:24:38 +0300

    Profile controller updated with instance variables for sent and favorite messages.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,8 @@
 class ProfileController < ApplicationController
   before_action :authenticate_user! #, :except => [:show, :index]
   def index
+    @sent = Message.where(sender_id:current_user.id)
     @user = User.find(current_user.id)
+    @favorites = @user.messages.where(favorite:true)  
   end
 end
