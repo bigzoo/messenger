36b7e76035c7333902aed2b63c23d1965050de3a
Chris Nyaga <nyagachris75@gmail.com>
2017-08-13 12:25:19 +0300

    Option for using subdomains added.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :profile, only: :index
+  # Uncomment below line to use subdomains instead of branches
+  # get '', to: 'users#show', constraints: lambda { |r| r.subdomain.present? && r.subdomain != 'www' }
   root 'main#index'
   resources :messages, only: :create
   get '/:username', to: 'users#show'
