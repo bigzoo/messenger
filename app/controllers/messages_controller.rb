2f9f4ffa42d84dc28cdec03855d6d038f9c0cb1d
Chris Nyaga <nyagachris75@gmail.com>
2017-08-13 13:53:59 +0300

    Delete method added to messages controller.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,13 +10,24 @@ class MessagesController < ApplicationController
     end
   end
 
+  def destroy
+    @message = Message.find(params[:id])
+    if @message.delete
+      flash[:notice]= 'Message Deleted Succesfully'
+      redirect_to profile_index_path
+    else
+      flash[:alert]= 'An Error Occured. Please Try Again'
+      redirect_to profile_index_path
+    end
+  end
+
   private
   def message_params
     sender_id_h = {sender_id:current_user.id} if user_signed_in?
     if sender_id_h
-      params.require(:message).permit(:user_id,:message).merge(sender_id_h)
+      params.require(:message).permit(:user_id,:message,:hint).merge(sender_id_h)
     else
-      params.require(:message).permit(:user_id,:message)
+      params.require(:message).permit(:user_id,:message,:hint)
     end
   end
 end
