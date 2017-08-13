97a7b073ec774d5750b026171721474a27064202
Chris Nyaga <nyagachris75@gmail.com>
2017-08-13 14:13:14 +0300

    Updated DB Schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811134854) do
+ActiveRecord::Schema.define(version: 20170813094650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170811134854) do
     t.boolean "favorite", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "seen", default: false
+    t.string "hint"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -46,6 +48,9 @@ ActiveRecord::Schema.define(version: 20170811134854) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "full_name"
+    t.boolean "public_profile", default: true
+    t.boolean "public_messages", default: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
